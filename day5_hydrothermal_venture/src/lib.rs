dda3fe65fb074d91614d22aa5b747a22c75d4144
Jan-Pieter Cornet <johnpc@xs4all.nl>
2023-04-07 18:02:42 +0200

    solve day5 part1

--- day5_hydrothermal_venture/src/lib.rs
+++ day5_hydrothermal_venture/src/lib.rs
@@ -1,7 +1,82 @@
 use exrunner::ExRunner;
-use std::{io, io::BufRead, collections::{HashMap, hash_map::Entry}};
+use std::{io::BufRead, str::FromStr, collections::HashMap};
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ParsePointError;
+
+impl FromStr for Point {
+    type Err = ParsePointError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let (xstr, ystr) = input.split_once(',').ok_or(ParsePointError)?;
+        let x: i32 = xstr.trim().parse().map_err(|_| ParsePointError)?;
+        let y: i32 = ystr.trim().parse().map_err(|_| ParsePointError)?;
+        Ok(Point {x, y})
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Line {
+    start: Point,
+    end: Point,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ParseLineError;
+
+impl FromStr for  Line {
+    type Err = ParseLineError;
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let (startstr, endstr) = input.split_once("->").ok_or(ParseLineError)?;
+        let start: Point = startstr.trim().parse().map_err(|_| ParseLineError)?;
+        let end: Point = endstr.trim().parse().map_err(|_| ParseLineError)?;
+        Ok(Line { start, end })
+    }
+}
+
+fn parse_input(input: impl BufRead) -> Vec<Line> {
+    input.lines().map(|l| l.expect("Error reading input").parse().expect("Error parsing input")).collect()
+}
 
 pub fn solve(input: impl BufRead, er: &mut ExRunner) {
+    let lines = parse_input(input);
+    er.parse_done();
+    // determine overlap for part1
+    let mut online: HashMap<Point, bool> = HashMap::new();
+    let mut double_points = 0;
+    for l in lines {
+        let mut x = l.start.x;
+        let mut y = l.start.y;
+        let x2 = l.end.x;
+        let y2 = l.end.y;
+        let dx; let dy;
+        if x == x2 {
+            dx = 0;
+            dy = if y > y2 { -1 } else { 1 };
+        } else if y == y2 {
+            dx = if x > x2 { -1 } else { 1 };
+            dy = 0;
+        } else {
+            continue;
+        }
+        loop {
+            online.entry(Point{ x, y})
+                .and_modify(|taken| if *taken { double_points += 1; *taken = false; })
+                .or_insert(true);
+            if x == x2 && y == y2 {
+                break;
+            }
+            x += dx;
+            y += dy;
+        }
+    }
+    er.part1(double_points, None);
 }
 
 #[cfg(test)]
